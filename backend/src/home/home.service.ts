7c738128d8c7f7080ed3601d9a1f128d3e90becd
hungnkb <hungnkb@gmail.com>
2023-04-12 00:41:31 +0700

    fix home query

--- backend/src/home/home.service.ts
+++ backend/src/home/home.service.ts
@@ -10,13 +10,13 @@ export class HomeService {
     constructor(
         @Inject('HOME_REPOSITORY')
         private homeRepository: Repository<HomeSchema>,
-        private userService: UserService
+        private userService: UserService,
     ) { }
 
     async create(body: CreateHomeDto): Promise<Object> {
         let { title, price, address, bathrooms, bedrooms, description, email, idCategory } = body
         let user = await this.userService.findByObj(email);
-        if (!user) {
+        if (!user || user.role != 'host') {
             throw new HttpException('Unauthorized', HttpStatus.BAD_REQUEST)
         }
         let newHome = await this.homeRepository
@@ -29,27 +29,27 @@ export class HomeService {
         return newHome
     }
 
-    async findAll(): Promise<HomeSchema[]> {
-        return this.homeRepository.find()
-    }
-
     async findByObj(obj): Promise<HomeSchema[] | undefined> {
         if (obj.idUser) {
             return this.homeRepository
                 .createQueryBuilder('homes')
-                .leftJoinAndSelect('homes.idUser', 'users')
+                .select(['homes', 'users.idUser', 'users.email', 'users.phone', 'users.image'])
+                .leftJoin('homes.idUser', 'users')
                 .where('homes.idUser = :id', { id: obj.idUser })
                 .getMany()
         } else if (obj.idHome) {
             return this.homeRepository
                 .createQueryBuilder('homes')
-                .leftJoinAndSelect('homes.idUser', 'users')
+                .select(['homes', 'users.idUser', 'users.email', 'users.phone', 'users.image'])
+                .leftJoin('homes.idUser', 'users')
                 .where('idHome = :id', { id: obj.idHome })
                 .getMany()
         } else {
             return this.homeRepository
                 .createQueryBuilder('homes')
-                .leftJoinAndSelect('homes.idUser', 'users')
+                .select(['homes', 'users.idUser.idUser', 'users.idUser.email', 'users.idUser.phone', 'users.idUser.image'])
+                .leftJoin('homes.idUser', 'users.idUser')
+                .leftJoinAndSelect('homes.idCategory', 'categories.idCateogry')
                 .getMany()
         }
     }

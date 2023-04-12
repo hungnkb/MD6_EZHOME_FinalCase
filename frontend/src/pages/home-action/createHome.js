270b21418ad3a84ec62a71fa48cf46ef5316f772
phithithuhuong <thu872315@gmail.com>
2023-04-12 08:14:37 +0700

    up

---
+++ frontend/src/pages/home-action/createHome.js
@@ -0,0 +1,36 @@
+import Form from "react-bootstrap/Form";
+import React from "react";
+
+export default function CreateHome(){
+    return (
+        <>
+            <Form>
+                <Form.Group className="mb-3" md="4" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="name@example.com"
+                        autoFocus
+                    />
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                >
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Form.Select aria-label="Default select example">
+                    <option>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </Form.Select>
+                <Form.Group controlId="formFileMultiple" className="mb-3">
+                    <Form.Label>Multiple files input example</Form.Label>
+                    <Form.Control type="file" multiple />
+                </Form.Group>
+            </Form>
+        </>
+    )
+}
--- frontend/src/pages/home-action/modalHome.js
+++ frontend/src/pages/home-action/modalHome.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import CreateHome from "./createHome";
 
  export default function ModalHome() {
     const [show, setShow] = useState(false);
@@ -11,7 +12,7 @@ import Modal from 'react-bootstrap/Modal';
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
+            <Button variant="warning" onClick={handleShow}>
                 Launch demo modal
             </Button>
 
@@ -20,30 +21,14 @@ import Modal from 'react-bootstrap/Modal';
                     <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="name@example.com"
-                                autoFocus
-                            />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlTextarea1"
-                        >
-                            <Form.Label>Example textarea</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
-                        </Form.Group>
-                    </Form>
+                    <CreateHome/>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+                    <Button variant="warning" onClick={handleClose}>
+                        Save
                     </Button>
                 </Modal.Footer>
             </Modal>

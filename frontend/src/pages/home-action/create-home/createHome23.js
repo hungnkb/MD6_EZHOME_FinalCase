5d3b1fbb89b1fc8e55fa3f77804ce18a039b3a6d
hungnkb <hungnkb@gmail.com>
2023-04-13 11:20:04 +0700

    create-home

--- frontend/src/pages/home-action/create-home/createHome23.js
+++ frontend/src/pages/home-action/create-home/createHome23.js
@@ -1,10 +1,12 @@
-import * as React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useDropzone } from 'react-dropzone';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -15,32 +17,46 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function CreateHome23() {
-    const navigate = useNavigate()
+    const [images, setImages] = useState([]);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const currentState = useSelector(state => state.createHome);
+    const [check, setCheck] = useState(false)
+
+    const onDrop = useCallback(acceptedFiles => {
+        setImages(images => [...images, ...acceptedFiles]);
+    }, [])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    useEffect(() => {
+        if (images.length > 0) {
+            setCheck(true)
+        }
+    }, [images])
     return (
         <>
             <Box sx={{ flexGrow: 1, marginTop: '50px', marginBottom: '50px' }}>
                 <Grid container spacing={2} style={{ marginLeft: '5%', marginRight: '5%' }}>
-                    <Grid xs={6}>
-                        <p style={{ fontSize: '70px' }}>It’s easy to get started on EZHOME</p>
-                    </Grid>
-                    <Grid xs={6}>
-                        <Item>
-                            <h1>1 Tell us about your place</h1>
-                            <p>Share some basic info, like where it is and how many guests can stay.</p>
-                        </Item>
-                        <Item>
-                            <h1>2 Make it stand out</h1>
-                            <p>Share some basic info, like where it is and how many guests can stay.</p>
-                        </Item>
-                        <Item>
-                            <h1>3 Finish up and publish</h1>
-                            <p>Share some basic info, like where it is and how many guests can stay.</p>
-                        </Item>
-                    </Grid>
+                    <h1>Make your home more beautiful</h1>
+
+                    <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        {
+                            isDragActive ?
+                                <p>Drop the files here ...</p> :
+                                <p>Drag 'n' drop some files here, or click to select files</p>
+                        }
+                    </div>
+                    <div style={{ display: 'flex', direction: 'ltr' }}>
+                        {images.map((image, i) => (
+                            <img style={{ width: '150px' }} key={i} src={URL.createObjectURL(image)} />
+                        ))}
+                    </div>
                 </Grid>
                 <Grid style={{ marginLeft: '5%', marginRight: '5%' }}>
                     <Button onClick={() => navigate('/create-home2/2')} variant="contained">Back</Button>
-                    <Button  variant="contained">Finish</Button>
+                    {check ? (<Button onClick={() => navigate('/create-home2/4')} variant="contained">Next</Button>) : null}
                 </Grid>
             </Box>
         </>
--- frontend/src/pages/home-action/create-home/createHome24.js
+++ frontend/src/pages/home-action/create-home/createHome24.js
@@ -1,10 +1,12 @@
-import * as React from 'react';
+import React, { useState, useCallback } from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useDropzone } from 'react-dropzone';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,8 +16,17 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-export default function CreateHome2() {
-    const navigate = useNavigate()
+export default function CreateHome24() {
+    const [images, setImages] = useState([]);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const currentState = useSelector(state => state.createHome);
+
+    const onDrop = useCallback(acceptedFiles => {
+        setImages(images => [...images, ...acceptedFiles]);
+    }, [])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
     return (
         <>
             <Box sx={{ flexGrow: 1, marginTop: '50px', marginBottom: '50px' }}>
@@ -24,22 +35,13 @@ export default function CreateHome2() {
                         <p style={{ fontSize: '70px' }}>It’s easy to get started on EZHOME</p>
                     </Grid>
                     <Grid xs={6}>
-                        <Item>
-                            <h1>1 Tell us about your place</h1>
-                            <p>Share some basic info, like where it is and how many guests can stay.</p>
-                        </Item>
-                        <Item>
-                            <h1>2 Make it stand out</h1>
-                            <p>Share some basic info, like where it is and how many guests can stay.</p>
-                        </Item>
-                        <Item>
-                            <h1>3 Finish up and publish</h1>
-                            <p>Share some basic info, like where it is and how many guests can stay.</p>
-                        </Item>
+                        <h1>Some more information about your home</h1>
+
                     </Grid>
                 </Grid>
                 <Grid style={{ marginLeft: '5%', marginRight: '5%' }}>
-                    <Button onClick={() => navigate('/create-home2/1')} variant="contained">Get started</Button>
+                    <Button onClick={() => navigate('/create-home2/3')} variant="contained">Back</Button>
+                    <Button variant="contained">Next</Button>
                 </Grid>
             </Box>
         </>

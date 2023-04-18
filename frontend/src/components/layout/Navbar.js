5180fd63a364198e75e70a14c387e5466003aaed
phithithuhuong <thu872315@gmail.com>
2023-04-18 11:10:53 +0700

    fix template

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -244,8 +244,8 @@ export default function Navbar() {
               <Search
                   style={{
                     border: '2px solid black',
-                    marginLeft: '300px',
-                    borderRadius: '30px',
+                    marginLeft: '30%',
+                    borderRadius: '20px',
                     width: 400,
                   }}
               >

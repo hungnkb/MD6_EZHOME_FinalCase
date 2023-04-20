2fb506f6c562ccf94e37255290294a5d445f1f14
PhuocMai31 <Phuwowsc@gmail.com>
2023-04-20 08:30:15 +0700

    fix route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -68,7 +68,7 @@ function App() {
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/user/hosting" element={<DashboardHosting />} />
         <Route path="/user/profile" element={<UpdateUser />} />
-          <Route path="/test" element={<HomeRenting />}></Route>
+          <Route path="/user/home" element={<HomeRenting />}></Route>
           <Route path='/homes' element={<CardHome />}></Route>
         </Route >
     <Route

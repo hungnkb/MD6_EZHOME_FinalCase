e436dd36cdf4f1461fc590f4a76fbc9c99d30581
PhuocMai31 <Phuwowsc@gmail.com>
2023-05-04 15:51:59 +0700

    pull

--- frontend/src/pages/hosting/dashboard.js
+++ frontend/src/pages/hosting/dashboard.js
@@ -45,11 +45,13 @@ function DashboardHosting() {
     const coupon = () => {
       let dateNow = new Date();
       dateNow = dateNow.getTime();
+      console.log(currentAuth.userLogin.sub,1313)
       axios
         .get(
           `http://localhost:3002/api/v1/coupons?idUser=${currentAuth.userLogin.sub}`,
         )
         .then((res) => {
+          console.log(res,2121)
           let newListCoupon = [];
           for (let i = 0; i < res.data.length; i++) {
             let startDate = new Date(res.data[i].startDate);
@@ -65,6 +67,7 @@ function DashboardHosting() {
     };
     coupon();
   }, []);
+  // console.log(listCoupon,2121)
   const handleChangeCoupon = (e, index) => {
     setAddCoupon(e.target.value);
     //id nha, id coupon
@@ -112,7 +115,7 @@ function DashboardHosting() {
     };
     getDataHome();
   }, [currentAuth.isLogined, flag]);
-  console.log(homeList, 22);
+
   return (
     <>
       <br />
